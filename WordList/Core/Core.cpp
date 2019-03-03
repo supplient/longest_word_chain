56dce5e129e99d6fb3d41b16b28619a49cf792ab
wmlbuaa <wmlbuaa@163.com>
2019-03-03 21:29:28 +0800

    初步添加DFS版本的Core，与框架函数未融合。

--- WordList/Core/Core.cpp
+++ WordList/Core/Core.cpp
@@ -4,6 +4,56 @@
 #include "stdafx.h"
 
 #include "Core.h"
+#include<algorithm>
+#include<iterator>
+
+using namespace GET_CHAIN;
+
+
+int Core::CreateMap(std::string s, int *degree) {
+	degree[s[0] - 'a']++;
+	degree[s[s.length() - 1] - 'a']++;
+	Edge edge = {s, 1, s[s.length() - 1] - 'a' };
+	map[s[0] - 'a'].toLast.push_back(edge);
+	return 0;
+}
+
+int Core::Recursion(std::vector<std::string>& path, int length, int point) {
+	if (map[point].toLast.empty()) {
+		if (length > MaxLen) {
+			MaxLen = length;
+			MaxPath.swap(path);
+		}
+		else {
+			path.clear();
+		}
+		return length;
+	}
+	for (auto iter : map[point].toLast) {
+		path.push_back(iter.word);
+		std::cout << point << " "<< iter.next << std::endl;
+		if(iter.next==iter)
+		Recursion(path, ++length, iter.next);
+	}
+	return 0;
+}
+
+void Core::get_max_chain(std::vector<std::string>& input) {
+	int i, degree[26] = { 0 }, maxDegree = 0;
+	std::vector<std::string> path;
+	for (i = 0; i < input.size(); i++) {
+		transform(input[i].begin(), input[i].end(), input[i].begin(), ::tolower);
+		CreateMap(input[i], degree);
+	}
+	for (i = 0; i < 26; i++) {
+		Recursion(path, 0, i);
+	}
+	
+	std::cout << MaxPath.size() << std::endl;
+	for (auto iter : MaxPath) {
+		std::cout << iter<<std::endl;
+	}
+}
 
 int Core::gen_chain_word(char * words[], int len, char * result[], char head, char tail, bool enable_loop)
 {
--- WordList/Core/Core.h
+++ WordList/Core/Core.h
@@ -1,4 +1,7 @@
 #pragma once
+#include<iostream>
+#include<string>
+#include<vector>
 
 #ifdef CORE_EXPORTS
 #define CORE_API __declspec(dllexport)
@@ -6,8 +9,28 @@
 #define CORE_API __declspec(dllimport)
 #endif
 
-class CORE_API Core {
-public:
-	static int gen_chain_word(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
-	static int gen_chain_char(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
-};
+namespace GET_CHAIN{
+	struct Edge {
+		std::string word;
+		int weight;
+		int next;
+	};
+	struct WordMap {
+		std::vector<Edge> toLast;
+		std::vector<Edge> toFirst;
+	};
+
+	class CORE_API Core {
+		private:
+			int MaxLen=0;
+			bool isSelfCircleUsed[26] = { 0 };
+			std::vector<std::string> MaxPath;
+			WordMap map[26];
+			int CreateMap(std::string s, int *degree);
+			int Recursion(std::vector<std::string>& path, int length, int point);
+		public:
+			void get_max_chain(std::vector<std::string>& input);
+			static int gen_chain_word(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
+			static int gen_chain_char(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
+	};
+}
