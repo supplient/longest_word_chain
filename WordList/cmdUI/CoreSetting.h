ba46a2971b8a2ca5e6f449f60dc5c0f72c1ea143
supplient <437566830@qq.com>
2019-03-03 13:51:54 +0800

    Add UnitTest's frame.

--- WordList/cmdUI/CoreSetting.h
+++ WordList/cmdUI/CoreSetting.h
@@ -1,12 +1,20 @@
 #pragma once
 #include <string>
 
-struct CoreSetting {
-public:
+class CoreSetting {
+private:
 	std::string filename;
 	bool max_word = false, max_char = false;
 	char head = 0, tail = 0;
 	bool enable_loop = false;
 
+public:
 	void parseFromArgc(int argc, char *argv[]);
+
+	std::string getFilename()const { return filename; }
+	bool isMaxWord()const { return max_word; }
+	bool isMaxChar()const { return max_char; }
+	char getHead()const { return head; }
+	char getTail()const { return tail; }
+	bool isLoopEnable()const { return enable_loop; }
 };
