ca2aee5aebef2e9c4fa87fb80dc22c53f8b53fea
ErrorOverflow <32595386+ErrorOverflow@users.noreply.github.com>
2019-03-05 16:49:04 +0800

    Core max char (#4)
    * del useless var "maxDegreee"
    
    * can get max char chain now
    
    * complete max char

--- WordList/Core/ChainSolver.cpp
+++ WordList/Core/ChainSolver.cpp
@@ -3,11 +3,15 @@
 
 #include <cstring>
 
-int ChainSolver::CreateMap(char* c_s) {
+int ChainSolver::CreateMap(char* c_s, bool isGetMaxChar) {
 	std::string s(c_s);
+	Edge edge;
 	if (s[0] == s[s.length() - 1])
 		isSelfCircleUsed[s[0]]++;
-	Edge edge = {s, 1, s[s.length() - 1] - 'a' };
+	if(isGetMaxChar)
+		edge = {s, 1, s[s.length() - 1] - 'a' };
+	else
+		edge = {s, (int)s.length(), s[s.length() - 1] - 'a' };
 	map[s[0] - 'a'].toLast.push_back(edge);
 	return 0;
 }
@@ -18,7 +22,7 @@ int ChainSolver::Recursion(std::vector<std::string>& path, int length, int point
 			maxLen = length;
 			maxPath.assign(path.begin(), path.end());
 		}
-		return length; // NOTE this return value seems to be nonsense.
+		return 0; // NOTE this return value seems to be nonsense. ANS: change to 0
 	}
 	for (auto iter : map[point].toLast) {
 		path.push_back(iter.word);
@@ -28,7 +32,8 @@ int ChainSolver::Recursion(std::vector<std::string>& path, int length, int point
 				isSelfCircleUsed[point]--;
 			else continue;
 		}
-		Recursion(path, length+1, iter.next);
+		//NOTE: when needing max num: weight=1, when needing max char: weight = word's length.
+		Recursion(path, length+iter.weight, iter.next);
 		if(iter.next == point)
 			isSelfCircleUsed[point]++;
 		path.pop_back();
@@ -36,12 +41,12 @@ int ChainSolver::Recursion(std::vector<std::string>& path, int length, int point
 	return 0;
 }
 
-int ChainSolver::get_max_chain(char* input[], int num, char* result[]) {
-	int i, maxDegree = 0; // NOTE: maxDegree seems to be an useless remaining variable?
+int ChainSolver::get_max_chain(char* input[], int num, char* result[], bool isGetMaxChar) {
+	int i; // NOTE: maxDegree seems to be an useless remaining variable? ANS: yes, it has been deleted.
 	std::vector<std::string> path;
 
 	for (i = 0; i < num; i++) {
-		CreateMap(input[i]);
+		CreateMap(input[i], isGetMaxChar);
 	}
 	for (i = 0; i < 26; i++) {
 		Recursion(path, 0, i);
--- WordList/CoreTest/ChainSolverTest.cpp
+++ WordList/CoreTest/ChainSolverTest.cpp
@@ -22,9 +22,9 @@ namespace CoreTest
 			};
 			char* res[4];
 
-			int res_len = Core::gen_chain_word(words, 4, res, 0, 0, 0);
+			int res_len = Core::gen_chain_char(words, 4, res, 0, 0, 0);
 
-			Assert::AreEqual(res_len, 4);
+			Assert::AreEqual(res_len, 4); 
 			Assert::AreEqual(res[0], "hello");
 			Assert::AreEqual(res[1], "ofk");
 			Assert::AreEqual(res[2], "kw");
