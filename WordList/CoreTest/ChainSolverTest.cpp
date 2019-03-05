ef95c2307917a1ff97b8709901d24a478dc29127
supplient <437566830@qq.com>
2019-03-05 15:28:07 +0800

    Add general test right function.

--- WordList/CoreTest/ChainSolverTest.cpp
+++ WordList/CoreTest/ChainSolverTest.cpp
@@ -2,14 +2,28 @@
 #include "CppUnitTest.h"
 
 #include "../Core/Core.h"
+#include <vector>
+#include <string>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
 
 namespace CoreTest
 {		
 	TEST_CLASS(ChainSolverTest)
 	{
+	private:
+		void testRight(char* words[], int words_len, char* res[], int res_len, 
+							char head=0, char tail=0, bool enable_loop=false) {
+			char** ret_res = new char*[words_len];
+			int ret_len = Core::gen_chain_word(words, words_len, ret_res, head, tail, enable_loop);
+
+			Assert::AreEqual(ret_len, res_len);
+			for (int i = 0; i < res_len; i++)
+				Assert::AreEqual(0, strcmp(ret_res[i], res[i]));
+		}
+
 	public:
 		
 		TEST_METHOD(simple)
@@ -20,15 +34,14 @@ namespace CoreTest
 				"ofk",
 				"kw"
 			};
-			char* res[4];
+			char* res[] = {
+				"hello",
+				"ofk",
+				"kw",
+				"world"
+			};
 
-			int res_len = Core::gen_chain_word(words, 4, res, 0, 0, 0);
-
-			Assert::AreEqual(res_len, 4);
-			Assert::AreEqual(res[0], "hello");
-			Assert::AreEqual(res[1], "ofk");
-			Assert::AreEqual(res[2], "kw");
-			Assert::AreEqual(res[3], "world");
+			testRight(words, 4, res, 4);
 		}
 
 	};
