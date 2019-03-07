f7f5858954149392f0adbab4bd1a10020d1bf5ef
supplient <437566830@qq.com>
2019-03-07 11:10:58 +0800

    Build PyramidGenerator to generate large testcase. (#8)
    * Build PyramidGenerator's interface.
    
    * Complete PyramidGenerator.

--- WordList/CoreTest/ChainSolverTest.cpp
+++ WordList/CoreTest/ChainSolverTest.cpp
@@ -2,8 +2,10 @@
 #include "CppUnitTest.h"
 
 #include "testutil.h"
+#include "PyramidGenerator.h"
 #include "../Core/Core.h"
 
+#include <fstream>
 #include <vector>
 #include <string>
 
@@ -31,6 +33,34 @@ namespace CoreTest
 			};
 
 			testRight(words, 4, res, 4);
+		}
+
+		TEST_METHOD(large) {
+			PyramidGenerator gen(30, 5);
+			gen.work();
+
+			char** words = gen.getWords();
+			int word_len = gen.getWordLen();
+			char** res = gen.getRes();
+			int res_len = gen.getResLen();
+
+			// save the words and res into temp files for recurring.
+			ofstream words_out("../testcase/words_temp.txt");
+			for (int i = 0; i < word_len; i++) {
+				words_out << words[i] << endl;
+			}
+			words_out.close();
+			ofstream res_out("../testcase/res_temp.txt");
+			for (int i = 0; i < res_len; i++) {
+				res_out << res[i] << endl;
+			}
+
+			testRight(words, word_len, res, res_len);
+
+			for (int i = 0; i < word_len; i++)
+				delete[] words[i];
+			delete[] words;
+			delete[] res;
 		}
 	};
 
